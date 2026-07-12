407abffe613ea8137137ba4c3077c37bae925533
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-07-12 05:33:02 +0500

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smit-final-hackathon-backend-nu.vercel.app/",
+  baseURL: "https://smit-final-hackathon-backend-nu.vercel.app/api",
 });
 
 api.interceptors.request.use(
